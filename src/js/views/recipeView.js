b1373b5438b7c17d0d8cfd5f6f042e9f2fa609af
Ricardo <ri.tapiasaez@gmail.com>
2024-08-24 22:19:58 -0400

    Implementing Error and Success Messages Lecture

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,10 +5,13 @@ import { Fraction } from "fractional";
 class RecipeView {
   #parentElement = document.querySelector(".recipe");
   #data;
+  #errorMessage = "We could not find the recipe. Please try another one!";
+  #message = "";
 
   render(data) {
     this.data = data;
     const markup = this.#generateMarkup();
+
     this.#clear();
     this.#parentElement.insertAdjacentHTML("afterbegin", markup);
   }
@@ -17,7 +20,7 @@ class RecipeView {
     this.#parentElement.innerHTML = "";
   }
 
-  renderSpinner = function () {
+  renderSpinner() {
     const markup = `
         <div class="spinner">
           <svg>
@@ -25,9 +28,44 @@ class RecipeView {
           </svg>
         </div>
     `;
-    this.#parentElement.innerHTML = "";
+    this.#clear();
     this.#parentElement.insertAdjacentHTML("afterbegin", markup);
-  };
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+        <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+        </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+        <div class="message">
+          <div>
+            <svg>
+              <use href="${icons}#icon-smile"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div>    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  addHandlerRender(handler) {
+    ["hashchange", "load"].forEach(ev => window.addEventListener(ev, handler));
+    // window.addEventListener("hashchange", controlRecipies);
+    // window.addEventListener("load", controlRecipies);
+  }
 
   #generateMarkup() {
     return `
